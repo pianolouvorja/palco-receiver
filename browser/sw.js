c930295e49f7ecb46964bc8c4cdb3d25c9c33acc
Rafael Dias Zendron <mmmarckos@gmail.com>
2026-09-04 12:23:30 -0300

    feat(receiver): fullscreen no primeiro clique

--- browser/sw.js
+++ browser/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'palco-receiver-v2'
+const CACHE = 'palco-receiver-v3'
 const SHELL = ['./', './index.html', './manifest.webmanifest', './icon.svg']
 
 self.addEventListener('install', (event) => {
